e5e6cebce711aca378af517b9ad27aee17f4cfee
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2021-10-29 05:49:38 +0200

    feat: Retain data across redeployments

--- hadoop-client/docker-entrypoint.d/10-mkdir-user-home.sh
+++ hadoop-client/docker-entrypoint.d/10-mkdir-user-home.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 
+install -d -o sandbox -g sandbox 755 /home/sandbox
 gosu hdfs hdfs dfs -mkdir -p /user/sandbox
 gosu hdfs hdfs dfs -chown sandbox:sandbox /user/sandbox
---
+++ hadoop-hdfs-datanode/docker-entrypoint.d/05-chmod-data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+chown root:hadoop /data
+chmod 775 /data
