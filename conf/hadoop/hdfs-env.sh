9726d1e9b311f3e0d34efc64bc883d2f4f3a45d7
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2022-10-03 16:30:19 +0200

    feat: Expose metrics in Prometheus format
    A number of additional changes have been implemented as well:
    
    * tiered networking with "front" and "clientnode" exposing services
    * JMX enabled on all Hadoop services
    * health checks for all containers
    * Removed instructions for docker-compose 1.x

---
+++ conf/hadoop/hdfs-env.sh
@@ -0,0 +1,2 @@
+export HDFS_NAMENODE_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=1028"
+export HDFS_DATANODE_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=1029"
